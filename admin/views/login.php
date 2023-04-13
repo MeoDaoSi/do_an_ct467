988553c174502f9f4df6dc062013f32b65ed6671
thub2014618 <thub2014618@student.ctu.edu.vn>
2023-04-13 19:39:38 +0700

    them public

---
+++ admin/views/login.php
@@ -0,0 +1,69 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/public/js/bootstrap.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script scr="/public/js/jquery-3.5.1.min.js"></script>
+<title>Document</title>
+</head>
+<body class="">
+
+
+<section class="vh-100" style="background-color: #85d192;">
+        <div class="container py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col col-xl-10">
+              <div class="card" style="border-radius: 1rem;">
+                <div class="row g-0">
+                  <div class="col-md-6 col-lg-5 d-none d-md-block">
+                    <img src="https://picfiles.alphacoders.com/244/244064.jpg" class="img-fluid" style="border-radius: 1rem 0 0 1rem; height: 550px;" />
+                  </div>
+                  <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                    <div class="card-body p-4 p-lg-5 text-black">
+      
+                      <form action="" method="">
+      
+                        <div class="d-flex align-items-center mb-3 pb-1">
+                          <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                          <span class="h1 fw-bold mb-0">Login</span>
+                        </div>
+      
+                        <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Đăng nhập</h5>
+      
+                        <div class="form-outline mb-4">
+                          <input type="email" id="dn_taikhoan" class="form-control form-control-lg" />
+                          <label class="form-label" for="dn_taikhoan" name="" >Tài khoản:</label>
+                        </div>
+      
+                        <div class="form-outline mb-4">
+                          <input type="password" id="dn_matkhau" class="form-control form-control-lg" />
+                          <label class="form-label" for="dn_matkhau" name="" >Mật khẩu:</label>
+                        </div>
+      
+                        <div class="pt-1 mb-4">
+                          <button class="btn btn-dark btn-lg btn-block"  type="submit">Đăng nhập</button>
+                        </div>
+      
+                        <a class="small text-muted" href="#!">Quên mật khẩu?</a>
+                        <p class="mb-5 pb-lg-2" style="color: #393f81;">Chưa có tài khoản? <a href="#!"
+                            style="color: #393f81;">Đăng ký</a></p>
+                        <a href="#!" class="small text-muted">Terms of use.</a>
+                        <a href="#!" class="small text-muted">Privacy policy</a>
+                      </form>
+      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+</body>
+</html>
